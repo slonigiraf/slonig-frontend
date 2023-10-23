bceab4de7f98164e8227e53b57291af730c5718d
Reshetov Denis <reshetovdenis@gmail.com>
2023-10-23 18:12:35 +0900

    Refactoring of home page

--- packages/page-home/src/index.tsx
+++ packages/page-home/src/index.tsx
@@ -2,92 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { AppProps as Props } from '@polkadot/react-components/types';
-
-import React, { useMemo } from 'react';
-import { Route, Routes } from 'react-router';
-
-import { Tabs } from '@polkadot/react-components';
-import { useApi } from '@polkadot/react-hooks';
-
-import I18n from './I18n/index';
-import Metadata from './Metadata/index';
-import Developer from './Developer';
-import General from './General';
+import React from 'react';
 import { useTranslation } from './translate.js';
 import useCounter from './useCounter.js';
 
 export { useCounter };
 
-function SettingsApp ({ basePath, onStatusChange }: Props): React.ReactElement<Props> {
+function SettingsApp (): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const { api, isApiConnected, isApiReady, isDevelopment } = useApi();
-  const numExtensions = useCounter();
-
-  const items = useMemo(() => [
-    {
-      isRoot: true,
-      name: 'general',
-      text: t('General')
-    },
-    {
-      count: numExtensions,
-      name: 'metadata',
-      text: t('Metadata')
-    },
-    {
-      name: 'developer',
-      text: t('Developer')
-    },
-    {
-      name: 'i18n',
-      text: t('Translate')
-    }
-  ], [numExtensions, t]);
-
-  const hidden = useMemo(
-    () => (isApiConnected && isApiReady)
-      ? isDevelopment || (api.runtimeMetadata.version <= 13)
-        ? []
-        : ['developer']
-      : ['metadata', 'i18n'],
-    [api, isApiConnected, isApiReady, isDevelopment]
-  );
-
+  
   return (
     <main className='settings--App'>
-      <Tabs
-        basePath={basePath}
-        hidden={hidden}
-        items={items}
-      />
-      <Routes>
-        <Route path={basePath}>
-          <Route
-            element={
-              <Developer onStatusChange={onStatusChange} />
-            }
-            path='developer'
-          />
-          <Route
-            element={
-              <I18n />
-            }
-            path='i18n'
-          />
-          <Route
-            element={
-              <Metadata />
-            }
-            path='metadata'
-          />
-          <Route
-            element={
-              <General />
-            }
-            index
-          />
-        </Route>
-      </Routes>
+      <h1>Put here buttons</h1>
     </main>
   );
 }
