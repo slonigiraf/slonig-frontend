39f9bd313504a2fff1d528c8f3bb841a7840d2e2
Reshetov Denis <reshetovdenis@gmail.com>
2023-10-23 18:20:06 +0900

    Button for scanning Qr

--- packages/page-home/src/index.tsx
+++ packages/page-home/src/index.tsx
@@ -5,15 +5,26 @@ import type { AppProps as Props } from '@polkadot/react-components/types';
 import React from 'react';
 import { useTranslation } from './translate.js';
 import useCounter from './useCounter.js';
+import { Button } from '@polkadot/react-components';
+import { useToggle } from '@polkadot/react-hooks';
 
 export { useCounter };
 
 function SettingsApp (): React.ReactElement<Props> {
   const { t } = useTranslation();
-  
+  const [isQrOpen, toggleQr] = useToggle();
+
   return (
     <main className='settings--App'>
-      <h1>Put here buttons</h1>
+      <div className='ui--row'>
+      <Button
+            icon='qrcode'
+            label={t('Scan Qr')}
+            onClick={toggleQr}
+          />
+      </div>
+      
+
     </main>
   );
 }
