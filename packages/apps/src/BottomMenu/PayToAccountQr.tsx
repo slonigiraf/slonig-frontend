094b1036aff3ca8b008cd217dc2d06c5fa4e6908
Reshetov Denis <reshetovdenis@gmail.com>
2023-10-29 23:44:25 +0900

    Fixed issue with wrong address display

--- packages/apps/src/BottomMenu/PayToAccountQr.tsx
+++ packages/apps/src/BottomMenu/PayToAccountQr.tsx
@@ -1,13 +1,30 @@
 import type { KeyringPair } from '@polkadot/keyring/types';
 import { keyring } from '@polkadot/ui-keyring';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { InputAddress } from '@polkadot/react-components';
 
 function PayToAccountQr(): React.ReactElement {
   const [currentPair, setCurrentPair] = useState<KeyringPair | null>(() => keyring.getPairs()[0] || null);
+  const _onChangeAccount = useCallback(
+    (accountId: string | null) => accountId && setCurrentPair(keyring.getPair(accountId)),
+    []
+  );
+
+  const hiddenAccountSelector = <div style={{ display: 'none' }}>
+    <InputAddress
+      className='full'
+      help={''}
+      isInput={false}
+      label={''}
+      onChange={_onChangeAccount}
+      type='account'
+    />
+  </div>;
 
   return (
     <>
-    <h2>Address: {currentPair.address}</h2>
+      {hiddenAccountSelector}
+      <h2>Address: {currentPair.address}</h2>
     </>
   );
 }
