27a22e57661c749c6ccfe39fd635e5bbbc924913
Reshetov Denis <reshetovdenis@gmail.com>
2023-11-10 23:26:49 +0900

    Refactoring

--- packages/page-recommendations/src/Employer/index.tsx
+++ packages/page-recommendations/src/Employer/index.tsx
@@ -2,12 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React, { useCallback, useState } from 'react';
-import { QRScanner } from '@slonigiraf/app-slonig-components';
 import InsurancesList from './InsurancesList';
 import { IPFS } from 'ipfs-core';
 import { useTranslation } from '../translate';
-import { InputAddress, Modal } from '@polkadot/react-components';
-import { storeInsurance } from '../utils';
+import { InputAddress } from '@polkadot/react-components';
 import type { KeyringPair } from '@polkadot/keyring/types';
 import { keyring } from '@polkadot/ui-keyring';
 import { u8aToHex } from '@polkadot/util';
@@ -20,52 +18,12 @@ interface Props {
 
 function Employer({ className = '', ipfs }: Props): React.ReactElement<Props> {
   const [currentPair, setCurrentPair] = useState<KeyringPair | null>(() => keyring.getPairs()[0] || null);
-  const [modalIsOpen, setModalIsOpen] = useState(false)
   const { t } = useTranslation();
 
   const _onChangeAccount = useCallback(
     (accountId: string | null) => accountId && setCurrentPair(keyring.getPair(accountId)),
     []
   );
-
-  const storeLetter = (data: string) => {
-    let dataArray = data.split(",")
-    if (dataArray.length === 11) {
-      storeToDB(dataArray);
-      setModalIsOpen(false);
-    }
-  }
-
-  const storeToDB = async (data: string[]) => {
-    const [textHash,
-      genesisHex,
-      letterId,
-      blockNumber,
-      refereePublicKeyHex,
-      workerPublicKeyHex,
-      amountValue,
-      refereeSignOverPrivateData,
-      refereeSignOverReceipt,
-      employerPublicKeyHex,
-      workerSignOverInsurance] = data;
-    
-    const insurance = {
-      created: new Date(),
-      cid: textHash,
-      genesis: genesisHex,
-      letterNumber: parseInt(letterId, 10),
-      block: blockNumber,
-      referee: refereePublicKeyHex,
-      worker: workerPublicKeyHex,
-      amount: amountValue,
-      signOverPrivateData: refereeSignOverPrivateData,
-      signOverReceipt: refereeSignOverReceipt,
-      employer: employerPublicKeyHex,
-      workerSign: workerSignOverInsurance,
-      wasUsed: false
-    };
-    storeInsurance(insurance);
-  }
 
   let publicKeyHex = "";
   if(currentPair !== null){
@@ -88,35 +46,9 @@ function Employer({ className = '', ipfs }: Props): React.ReactElement<Props> {
           onChange={_onChangeAccount}
         />
       </div>
-      {/* <div className='ui--row'>
-        <Button
-          icon='plus'
-          label={t('Add worker\'s letter')}
-          onClick={() => setModalIsOpen(true)}
-        />
-      </div> */}
       <div className='ui--row'>
         <InsurancesList ipfs={ipfs} employer={u8aToHex(currentPair?.publicKey)} />
       </div>
-
-      {modalIsOpen && <div className='ui--row'>
-        <Modal
-          header={t('Scan a letter QR code')}
-          onClose={() => setModalIsOpen(false)}
-          size='small'
-        >
-          <Modal.Content>
-            <QRScanner
-              onResult={(result, error) => {
-                if (result != undefined) {
-                  storeLetter(result?.getText())
-                }
-              }}
-              constraints={{facingMode: 'environment'}}
-            />
-          </Modal.Content>
-        </Modal>
-      </div>}
     </div>
   )
 }
