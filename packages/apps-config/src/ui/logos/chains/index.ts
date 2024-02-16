468126a29b4c6636574e3ce1cc2232cbe7d65090
Reshetov Denis <reshetovdenis@gmail.com>
2024-02-16 15:07:30 +0100

    Remove unused logos

--- packages/apps-config/src/ui/logos/chains/index.ts
+++ packages/apps-config/src/ui/logos/chains/index.ts
@@ -3,25 +3,6 @@
 
 // Do not edit. Auto-generated via node scripts/imgConvert.mjs
 
-export { chains3dpassSVG } from './generated/3dpassSVG.js';
-export { chainsAcalaSVG } from './generated/acalaSVG.js';
-export { chainsAlephSVG } from './generated/alephSVG.js';
-export { chainsAltairSVG } from './generated/altairSVG.js';
-export { chainsAmplitudeSVG } from './generated/amplitudeSVG.js';
-export { chainsDebioSVG } from './generated/debioSVG.js';
-export { chainsEquilibriumSVG } from './generated/equilibriumSVG.js';
-export { chainsFrequencySVG } from './generated/frequencySVG.js';
-export { chainsGenshiroSVG } from './generated/genshiroSVG.js';
 export { chainsGmJPEG } from './generated/gmJPEG.js';
-export { chainsIdiyanaleLogoWhiteSVG } from './generated/idiyanale_logo_whiteSVG.js';
 export { chainsInvarchJPEG } from './generated/invarchJPEG.js';
-export { chainsKaruraSVG } from './generated/karuraSVG.js';
-export { chainsKusamaSVG } from './generated/kusamaSVG.js';
-export { chainsPendulumSVG } from './generated/pendulumSVG.js';
-export { chainsPicassoSVG } from './generated/picassoSVG.js';
-export { chainsPolkadotCircleSVG } from './generated/polkadot-circleSVG.js';
-export { chainsRococoSVG } from './generated/rococoSVG.js';
 export { chainsSlonigSVG } from './generated/slonigSVG.js';
-export { chainsSnakenetSVG } from './generated/snakenetSVG.js';
-export { chainsTotemSVG } from './generated/totemSVG.js';
-export { chainsVaraSVG } from './generated/varaSVG.js';
