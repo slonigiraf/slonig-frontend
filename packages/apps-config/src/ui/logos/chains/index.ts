0bbc202612bc96dc7d46bdca57ad38ab6dd24cb1
Reshetov Denis <reshetovdenis@gmail.com>
2024-02-16 15:09:18 +0100

    Remove unused logos

--- packages/apps-config/src/ui/logos/chains/index.ts
+++ packages/apps-config/src/ui/logos/chains/index.ts
@@ -3,6 +3,4 @@
 
 // Do not edit. Auto-generated via node scripts/imgConvert.mjs
 
-export { chainsGmJPEG } from './generated/gmJPEG.js';
-export { chainsInvarchJPEG } from './generated/invarchJPEG.js';
 export { chainsSlonigSVG } from './generated/slonigSVG.js';
