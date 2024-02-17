216a57b936e14c3482d8558ecab12b69134581ef
Reshetov Denis <reshetovdenis@gmail.com>
2024-02-17 12:50:36 +0100

    Removes electron

--- packages/apps-electron/src/main/ipc-main-handler.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2023 @polkadot/apps authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export type IpcMainHandler = {
-  [channel: string]: (...args: any[]) => unknown
-}
