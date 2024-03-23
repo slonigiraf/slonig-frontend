25a87547293e7a9c1c72cfdb7b758648d2cee8c6
Reshetov Denis <reshetovdenis@gmail.com>
2024-03-23 12:37:22 +0100

    Can edit skills with images

--- packages/page-laws/src/Edit/ExerciseEditor.tsx
+++ packages/page-laws/src/Edit/ExerciseEditor.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import { TextArea } from '@polkadot/react-components';
+import React, { useState } from 'react';
+import { TextArea, styled } from '@polkadot/react-components';
 import { useTranslation } from '../translate.js';
 
-interface ExerciseEditorProps {
-  exercise: { h: string; a: string };
-  index: number;
-  list: any;
-  onListChange: (updatedList: any) => void;
-}
-
-const ExerciseEditor: React.FC<ExerciseEditorProps> = ({ exercise, index, list, onListChange }) => {
+const ExerciseEditor = ({ exercise, index, list, onListChange }) => {
   const { t } = useTranslation();
+  const [exerciseImage, setExerciseImage] = useState(exercise.p || '');
+  const [solutionImage, setSolutionImage] = useState(exercise.i || '');
+
+  const convertToBase64 = (file, callback) => {
+    const reader = new FileReader();
+    reader.onload = (e) => callback(e.target.result);
+    reader.readAsDataURL(file);
+  };
+
+  const handleImageChange = (e, type) => {
+    const file = e.target.files[0];
+    if (file) {
+      convertToBase64(file, (base64Image) => {
+        if (type === 'exercise') {
+          setExerciseImage(base64Image);
+          updateExercise({ ...exercise, p: base64Image });
+        } else {
+          setSolutionImage(base64Image);
+          updateExercise({ ...exercise, i: base64Image });
+        }
+      });
+    }
+  };
 
   const onEditHeader = (newHeader: string) => {
     const updatedExercises = [...(list.q || [])];
@@ -24,22 +40,54 @@ const ExerciseEditor: React.FC<ExerciseEditorProps> = ({ exercise, index, list, 
     onListChange({ ...list, q: updatedExercises });
   };
 
+  const updateExercise = (updatedExercise) => {
+    const updatedExercises = [...(list.q || [])];
+    updatedExercises[index] = updatedExercise;
+    onListChange({ ...list, q: updatedExercises });
+  };
+
   return (
-    <div key={index} className="exercise-editor">
+    <div className="exercise-editor">
       <TextArea
-        className='full'
         label={t('Exercise')}
-        onChange={onEditHeader}
         seed={exercise.h}
+        onChange={onEditHeader}
       />
+      <ImageUploadContainer>
+        {exerciseImage && <StyledImage src={exerciseImage} alt="Exercise" />}
+        <input
+          type="file"
+          onChange={(e) => handleImageChange(e, 'exercise')}
+          accept="image/*"
+        />
+      </ImageUploadContainer>
+
       <TextArea
-        className='full'
         label={t('Solution')}
-        onChange={onEditAnswer}
         seed={exercise.a}
+        onChange={onEditAnswer}
       />
+      <ImageUploadContainer>
+        {solutionImage && <StyledImage src={solutionImage} alt="Solution" />}
+        <input
+          type="file"
+          onChange={(e) => handleImageChange(e, 'solution')}
+          accept="image/*"
+        />
+      </ImageUploadContainer>
     </div>
   );
-}
+};
 
+const ImageUploadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const StyledImage = styled.img`
+  width: 200px;
+  margin-bottom: 10px;
+`;
 export default React.memo(ExerciseEditor);
