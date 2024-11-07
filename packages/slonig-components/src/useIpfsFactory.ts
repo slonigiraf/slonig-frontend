bddf944be86d0c97fac5dc726362d441781d3c87
Reshetov Denis <reshetovdenis@gmail.com>
2024-11-07 12:01:19 +0100

    IpfsContext refactoring

--- packages/slonig-components/src/useIpfsFactory.ts
+++ packages/slonig-components/src/useIpfsFactory.ts
@@ -1,62 +1,45 @@
-// From https://github.com/ipfs-examples/js-ipfs-examples/blob/master/examples/browser-create-react-app/src/hooks/use-ipfs-factory.js
+import { useEffect, useState } from 'react';
+import { create, IPFSHTTPClient } from 'kubo-rpc-client';
 
-// import { create } from 'ipfs-core'
-import { useEffect, useState } from 'react'
-import { multiaddr } from '@multiformats/multiaddr'
-import { create } from 'kubo-rpc-client'
+let ipfs: IPFSHTTPClient | null = null;
 
-let ipfs = null
-
-/*
- * A quick demo using React hooks to create an ipfs instance.
- *
- * Hooks are brand new at the time of writing, and this pattern
- * is intended to show it is possible. I don't know if it is wise.
- *
- * Next steps would be to store the ipfs instance on the context
- * so use-ipfs calls can grab it from there rather than expecting
- * it to be passed in.
- */
-export default function useIpfsFactory () {
-  const [isIpfsReady, setIpfsReady] = useState(Boolean(ipfs))
-  const [ipfsInitError, setIpfsInitError] = useState(null)
+export default function useIpfsFactory() {
+  const [isIpfsReady, setIpfsReady] = useState(Boolean(ipfs));
+  const [ipfsInitError, setIpfsInitError] = useState<Error | null>(null);
 
   useEffect(() => {
-    // The fn to useEffect should not return anything other than a cleanup fn,
-    // So it cannot be marked async, which causes it to return a promise,
-    // Hence we delegate to a async fn rather than making the param an async fn.
+    async function initIpfs() {
+      if (ipfs) {
+        console.log('IPFS already started');
+        setIpfsReady(true);
+      } else if (window.ipfs?.enable) {
+        console.log('Found window.ipfs');
+        ipfs = await window.ipfs.enable({ commands: ['id'] });
+        setIpfsReady(Boolean(ipfs));
+      } else {
+        try {
+          console.time('IPFS Started');
+          ipfs = await create({ url: `https://${process.env.IPFS_SERVER}/api/v0` });
+          setIpfsReady(true);
+          console.timeEnd('IPFS Started');
+        } catch (error) {
+          console.error('IPFS init error:', error);
+          setIpfsInitError(error as Error);
+        }
+      }
+    }
 
-    startIpfs()
-    return function cleanup () {
+    initIpfs();
+
+    return () => {
       if (ipfs && ipfs.stop) {
-        console.log('Stopping IPFS')
-        ipfs.stop().catch(err => console.error(err))
-        ipfs = null
-        setIpfsReady(false)
+        console.log('Stopping IPFS');
+        ipfs.stop().catch(err => console.error(err));
+        ipfs = null;
+        setIpfsReady(false);
       }
-    }
-  }, [])
+    };
+  }, []);
 
-  async function startIpfs () {
-    if (ipfs) {
-      console.log('IPFS already started')
-    } else if (window.ipfs && window.ipfs.enable) {
-      console.log('Found window.ipfs')
-      ipfs = await window.ipfs.enable({ commands: ['id'] })
-    } else {
-      try {
-        console.time('IPFS Started')
-        ipfs = create({ url: `https://${process.env.IPFS_SERVER}/api/v0` });
-        console.timeEnd('IPFS Started')
-      } catch (error) {
-        console.error('IPFS init error:', error)
-        ipfs = null
-        setIpfsInitError(error)
-      }
-    }
-
-    setIpfsReady(Boolean(ipfs))
-  }
-
-  return { ipfs, isIpfsReady, ipfsInitError }
+  return { ipfs, isIpfsReady, ipfsInitError };
 }
