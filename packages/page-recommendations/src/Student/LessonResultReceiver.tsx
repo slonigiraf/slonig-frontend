135d8fd24bdb1e501fef203092eaa2321ed572d5
Reshetov Denis <reshetovdenis@gmail.com>
2024-11-17 07:32:06 +0100

    Working sending bounty

--- packages/page-recommendations/src/Student/LessonResultReceiver.tsx
+++ packages/page-recommendations/src/Student/LessonResultReceiver.tsx
@@ -144,7 +144,7 @@ function LessonResultReceiver({ webRTCPeerId }: Props): React.ReactElement {
               if (letter) {
                 // generate a data to sign      
                 const letterInsurance = getDataToSignByWorker(letter.letterNumber, new BN(letter.block), new BN(letter.block), hexToU8a(letter.referee),
-                  hexToU8a(letter.worker), new BN(letter.amount), hexToU8a(letter.signOverReceipt), hexToU8a(letter.worker));
+                  hexToU8a(letter.worker), new BN(letter.amount), hexToU8a(letter.signOverReceipt), hexToU8a(lessonResultJson?.referee));
 
                 const diplomaKey = keyForCid(currentPair, letter.cid);
                 const workerSignOverInsurance = u8aToHex(diplomaKey.sign(u8aWrapBytes(letterInsurance)));
