4d0a8be6bbf82c63874ddf9cd3acf354fb9c2be7
Reshetov Denis <reshetovdenis@gmail.com>
2024-11-25 13:29:02 +0100

    Better formatting

--- packages/db/src/db/index.ts
+++ packages/db/src/db/index.ts
@@ -1,34 +1,34 @@
 import Dexie, { Table } from 'dexie';
-import { LetterTemplate } from './LetterTemplate.js';
-import { Letter } from './Letter.js';
-import { CanceledLetter } from './CanceledLetter.js';
+import { Agreement } from './Agreement.js';
 import { CanceledInsurance } from './CanceledInsurance.js';
-import { Pseudonym } from './Pseudonym.js';
-import { Signer } from './Signer.js';
-import { UsageRight } from './UsageRight.js';
+import { CanceledLetter } from './CanceledLetter.js';
+import { CIDCache } from './CIDCache.js';
 import { Insurance } from './Insurance.js';
+import { Lesson } from './Lesson.js';
+import { Letter } from './Letter.js';
+import { LetterTemplate } from './LetterTemplate.js';
+import { Pseudonym } from './Pseudonym.js';
+import { Reexamination } from './Reexamination.js';
 import { Reimbursement } from './Reimbursement.js';
 import { Setting } from './Setting.js';
-import { Lesson } from './Lesson.js';
-import { Agreement } from './Agreement.js';
-import { CIDCache } from './CIDCache.js';
-import { Reexamination } from './Reexamination.js';
+import { Signer } from './Signer.js';
+import { UsageRight } from './UsageRight.js';
 
 class SlonigirafDB extends Dexie {
+  agreements!: Table<Agreement>;
+  canceledInsurances!: Table<CanceledInsurance>;
+  canceledLetters!: Table<CanceledLetter>;
+  cidCache!: Table<CIDCache>;
+  insurances!: Table<Insurance>;
+  lessons!: Table<Lesson>;
   letters!: Table<Letter>;
   letterTemplates!: Table<LetterTemplate>;
-  canceledLetters!: Table<CanceledLetter>;
-  canceledInsurances!: Table<CanceledInsurance>;
   pseudonyms!: Table<Pseudonym>;
-  signers!: Table<Signer>;
-  usageRights!: Table<UsageRight>;
-  insurances!: Table<Insurance>;
-  reexaminations!: Table<Reexamination>; 
+  reexaminations!: Table<Reexamination>;
   reimbursements!: Table<Reimbursement>;
   settings!: Table<Setting>;
-  lessons!: Table<Lesson>;
-  agreements!: Table<Agreement>;
-  cidCache!: Table<CIDCache>;
+  signers!: Table<Signer>;
+  usageRights!: Table<UsageRight>;
 
   constructor() {
     super('slonig');
