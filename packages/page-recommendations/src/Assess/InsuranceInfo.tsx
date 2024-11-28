ab5472475e1ebdca33a98c2cdea62b045c921296
Reshetov Denis <reshetovdenis@gmail.com>
2024-11-28 16:55:57 +0100

    Refactoring

--- packages/page-recommendations/src/Assess/InsuranceInfo.tsx
+++ packages/page-recommendations/src/Assess/InsuranceInfo.tsx
@@ -1,16 +1,15 @@
 // Copyright 2021-2022 @slonigiraf/app-recommendations authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Modal, Button, Card, Spinner, styled } from '@polkadot/react-components';
+import { Modal, Button, Spinner, styled } from '@polkadot/react-components';
 import React, { useState, useEffect } from 'react'
 import UseInsurance from './UseInsurance.js'
 import { useTranslation } from '../translate.js';
-import { deleteInsurance, Insurance } from '@slonigiraf/db';
+import { Insurance, Letter } from '@slonigiraf/db';
 import { useToggle } from '@polkadot/react-hooks';
 import { Exercise, KatexSpan, getIPFSDataFromContentID, parseJson } from '@slonigiraf/app-slonig-components';
 import { useIpfsContext } from '@slonigiraf/app-slonig-components';
 import { ExerciseList } from '@slonigiraf/app-laws';
-import { useInfo } from '@slonigiraf/app-slonig-components';
 
 interface Props {
   className?: string;
@@ -28,8 +27,6 @@ function InsuranceInfo({ className = '', insurance, isSelected, onToggleSelectio
   const [areDetailsOpen, toggleDetailsOpen] = useToggle(false);
   const [skillName, setSkillName] = useState(insurance.cid);
   const [loaded, setLoaded] = useState(false);
-  const [isDeleteConfirmOpen, toggleDeleteConfirm] = useToggle();
-  const { showInfo } = useInfo();
 
   useEffect(() => {
     async function fetchData() {
@@ -52,29 +49,7 @@ function InsuranceInfo({ className = '', insurance, isSelected, onToggleSelectio
 
   const skillNameToShow = loaded ? <KatexSpan content={skillName} /> : <Spinner noLabel />;
 
-  const deleteButton = <Button
-    icon={'trash'}
-    label={t('')}
-    onClick={toggleDeleteConfirm}
-  />;
-
-  const deleteDiplomas = async () => {
-    try {
-      if (insurance && insurance.workerSign) {
-        await deleteInsurance(insurance.workerSign);
-        showInfo(t('Deleted'));
-      }
-    } catch (error) {
-      // Handle any errors that occur during the deletion process
-      console.error('Error', error);
-      // Optionally, show an error message to the user
-      showInfo(t('Deletion failed'));
-    } finally {
-      toggleDeleteConfirm();
-    }
-  };
-
-  data && data.e && data.e.map((item: Exercise, index: number) => {
+  data && data.e && data.e.map((item: Exercise, _index: number) => {
     console.log('item: ', JSON.stringify(item, null, 2))
   });
 
