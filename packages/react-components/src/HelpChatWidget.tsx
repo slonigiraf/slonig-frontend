e2d23754ccf923c9f7220803b1aacd0e39861c48
Reshetov Denis <reshetovdenis@gmail.com>
2025-04-30 18:05:32 +0200

    Hide help chat options when click outside

--- packages/react-components/src/HelpChatWidget.tsx
+++ packages/react-components/src/HelpChatWidget.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { styled } from '@polkadot/react-components';
 
-interface Props { }
+interface Props {
+  caption: string;
+}
 
 const Wrapper = styled.div`
   position: fixed;
@@ -81,72 +83,84 @@ const ToggleWrapper = styled.button`
   color: white;
 `;
 
-interface Props {
-    caption: string;
-}
-
 function HelpChatWidget({ caption }: Props): React.ReactElement<Props> {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-    const toggleChat = (): void => setIsOpen(!isOpen);
+  const toggleChat = (): void => setIsOpen(!isOpen);
 
-    const MessengerIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M256.6 8C116.5 8 8 110.3 8 248.6c0 72.3 29.7 134.8 78.1 177.9 8.4 7.5 6.6 11.9 8.1 58.2A19.9 19.9 0 0 0 122 502.3c52.9-23.3 53.6-25.1 62.6-22.7C337.9 521.8 504 423.7 504 248.6 504 110.3 396.6 8 256.6 8zm149.2 185.1l-73 115.6a37.4 37.4 0 0 1 -53.9 9.9l-58.1-43.5a15 15 0 0 0 -18 0l-78.4 59.4c-10.5 7.9-24.2-4.6-17.1-15.7l73-115.6a37.4 37.4 0 0 1 53.9-9.9l58.1 43.5a15 15 0 0 0 18 0l78.4-59.4c10.4-8 24.1 4.5 17.1 15.6z" />
-        </svg>
-    );
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
 
-    const WhatsAppIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
-        </svg>
-    );
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
 
-    const TelegramIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
+  const MessengerIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M256.6 8C116.5 8 8 110.3 8 248.6c0 72.3 29.7 134.8 78.1 177.9 8.4 7.5 6.6 11.9 8.1 58.2A19.9 19.9 0 0 0 122 502.3c52.9-23.3 53.6-25.1 62.6-22.7C337.9 521.8 504 423.7 504 248.6 504 110.3 396.6 8 256.6 8zm149.2 185.1l-73 115.6a37.4 37.4 0 0 1 -53.9 9.9l-58.1-43.5a15 15 0 0 0 -18 0l-78.4 59.4c-10.5 7.9-24.2-4.6-17.1-15.7l73-115.6a37.4 37.4 0 0 1 53.9-9.9l58.1 43.5a15 15 0 0 0 18 0l78.4-59.4c10.4-8 24.1 4.5 17.1 15.6z" />
+    </svg>
+  );
+
+  const WhatsAppIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+    </svg>
+  );
+
+  const TelegramIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
             <path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
-        </svg>
-    );
+    </svg>
+  );
 
-    const EmailIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+  const EmailIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
-        </svg>
-    );
+    </svg>
+  );
 
+  const ChatIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z" />
+    </svg>
+  );
 
-    const ChatIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z" />
-        </svg>
-    );
-
-    return (
-        <Wrapper>
-            {isOpen && (
-                <ChatOptions>
-                    <ChatHeader>{caption}</ChatHeader>
-                    <ChatButtonsRow>
-                        <ChatButtonWrapper href="https://m.me/reshetovdenis1" target="_blank" rel="noopener noreferrer" bg="#0078ff">
-                            {MessengerIcon}
-                        </ChatButtonWrapper>
-                        <ChatButtonWrapper href="https://wa.me/38267887600" target="_blank" rel="noopener noreferrer" bg="#25D366">
-                            {WhatsAppIcon}
-                        </ChatButtonWrapper>
-                        <ChatButtonWrapper href="https://t.me/denisreshetov" target="_blank" rel="noopener noreferrer" bg="#0088cc">
-                            {TelegramIcon}
-                        </ChatButtonWrapper>
+  return (
+    <Wrapper ref={wrapperRef}>
+      {isOpen && (
+        <ChatOptions>
+          <ChatHeader>{caption}</ChatHeader>
+          <ChatButtonsRow>
+            <ChatButtonWrapper href="https://m.me/reshetovdenis1" target="_blank" rel="noopener noreferrer" bg="#0078ff">
+              {MessengerIcon}
+            </ChatButtonWrapper>
+            <ChatButtonWrapper href="https://wa.me/38267887600" target="_blank" rel="noopener noreferrer" bg="#25D366">
+              {WhatsAppIcon}
+            </ChatButtonWrapper>
+            <ChatButtonWrapper href="https://t.me/denisreshetov" target="_blank" rel="noopener noreferrer" bg="#0088cc">
+              {TelegramIcon}
+            </ChatButtonWrapper>
                         <ChatButtonWrapper href="mailto:reshetovdenis@gmail.com" target="_blank" rel="noopener noreferrer" bg="#EA4335">
-                            {EmailIcon}
-                        </ChatButtonWrapper>
-                    </ChatButtonsRow>
-                </ChatOptions>
-            )}
-            <ToggleWrapper onClick={toggleChat}>
-                {isOpen ? '×' : ChatIcon}
-            </ToggleWrapper>
-        </Wrapper>
-    );
+              {EmailIcon}
+            </ChatButtonWrapper>
+          </ChatButtonsRow>
+        </ChatOptions>
+      )}
+      <ToggleWrapper onClick={toggleChat}>
+        {isOpen ? '×' : ChatIcon}
+      </ToggleWrapper>
+    </Wrapper>
+  );
 }
 
 export default React.memo(HelpChatWidget);
