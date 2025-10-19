859a3cdeeafc8149599879b0528ac44c57c67531
Reshetov Denis <reshetovdenis@gmail.com>
2025-10-19 22:07:50 +0200

    Ensures that we can use only settings written in SettingKey

--- packages/slonig-components/src/SettingsProvider.tsx
+++ packages/slonig-components/src/SettingsProvider.tsx
@@ -1,60 +1,26 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { getSetting, storeSetting, SettingKey } from '@slonigiraf/db';
 
-interface Settings {
-  ACCOUNT: string | undefined;
-  ENCRYPTION_KEY: string | undefined;
-  IV: string | undefined;
-  TUTOR: string | undefined;
-  LESSON: string | undefined;
-  RESULTS_FOR_LESSON: string | undefined;
-  DEVELOPER: string | undefined;
-  TEACHER: string | undefined;
-  DIPLOMA_PRICE: string | undefined;
-  DIPLOMA_WARRANTY: string | undefined;
-  DIPLOMA_VALIDITY: string | undefined;
-  INSURANCE_VALIDITY: string | undefined;
-  CID_CACHE_SIZE: string | undefined;
-  ECONOMY_INITIALIZED: string | undefined;
-  AIRDROP_COMPATIBLE: string | undefined;
-  RECEIVED_AIRDROP: string | undefined;
-  LESSON_RESULTS_ARE_SHOWN: string | undefined;
-  TUTOR_TUTORIAL_COMPLETED: string | undefined;
-  TUTEE_TUTORIAL_COMPLETED: string | undefined;
-  SCAN_TUTORIAL_COMPLETED: string | undefined;
-}
+// --- Derive type from SettingKey ---
+type SettingKeyType = typeof SettingKey;
+type SettingName = keyof SettingKeyType;
+
+interface Settings extends Record<SettingName, string | undefined> { }
 
 interface SettingsContextType {
   settings: Settings;
-  saveSetting: (key: keyof Settings, value: string) => Promise<void>;
+  saveSetting: (key: SettingName, value: string) => Promise<void>;
 }
 
-const defaultSettings: Settings = {
-  ACCOUNT: undefined,
-  ENCRYPTION_KEY: undefined,
-  IV: undefined,
-  TUTOR: undefined,
-  LESSON: undefined,
-  RESULTS_FOR_LESSON: undefined,
-  DEVELOPER: undefined,
-  TEACHER: undefined,
-  DIPLOMA_PRICE: undefined,
-  DIPLOMA_WARRANTY: undefined,
-  DIPLOMA_VALIDITY: undefined,
-  INSURANCE_VALIDITY: undefined,
-  CID_CACHE_SIZE: undefined,
-  ECONOMY_INITIALIZED: undefined,
-  AIRDROP_COMPATIBLE: undefined,
-  RECEIVED_AIRDROP: undefined,
-  LESSON_RESULTS_ARE_SHOWN: undefined,
-  TUTOR_TUTORIAL_COMPLETED: undefined,
-  TUTEE_TUTORIAL_COMPLETED: undefined,
-  SCAN_TUTORIAL_COMPLETED: undefined,
-};
+// --- Default values ---
+const defaultSettings = Object.keys(SettingKey).reduce((acc, key) => {
+  acc[key as SettingName] = undefined;
+  return acc;
+}, {} as Settings);
 
 const defaultContext: SettingsContextType = {
   settings: defaultSettings,
-  saveSetting: async () => { }
+  saveSetting: async () => {}
 };
 
 const SettingsContext = createContext<SettingsContextType>(defaultContext);
@@ -65,17 +31,14 @@ interface SettingsProviderProps {
 
 export const SettingsProvider: React.FC<SettingsProviderProps> = ({ children }) => {
   const [settings, _setSettings] = useState<Settings>(defaultSettings);
-
-  console.log('settings: ', settings)
-
+  
   useEffect(() => {
     (async () => {
-      const keys = Object.keys(defaultSettings) as (keyof Settings)[];
       const newState: Partial<Settings> = {};
 
-      for (const key of keys) {
+      for (const key of Object.keys(SettingKey) as SettingName[]) {
         try {
-          const settingKey = (SettingKey as any)[key] ?? key;
+          const settingKey = SettingKey[key];
           const storedValue = await getSetting(settingKey);
           if (storedValue !== undefined && storedValue !== null) {
             newState[key] = storedValue;
@@ -89,14 +52,13 @@ export const SettingsProvider: React.FC<SettingsProviderProps> = ({ children }) 
     })();
   }, []);
 
-  const setSettings = async (key: keyof Settings, value: string) => {
+  const saveSetting = async (key: SettingName, value: string) => {
     _setSettings(prev => ({ ...prev, [key]: value }));
-    const settingKey = (SettingKey as any)[key] ?? key;
-    await storeSetting(settingKey, value);
+    await storeSetting(SettingKey[key], value);
   };
 
   return (
-    <SettingsContext.Provider value={{ settings: settings, saveSetting: setSettings }}>
+    <SettingsContext.Provider value={{ settings, saveSetting }}>
       {children}
     </SettingsContext.Provider>
   );
