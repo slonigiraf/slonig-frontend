8ddb330df46d43458f06469f56d8b2bc550400c0
Reshetov Denis <reshetovdenis@gmail.com>
2026-01-25 14:00:30 +0100

    Single update.sh for both prod and dev

--- update.sh
+++ update.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
+set -e
+
+DIR_NAME="$(basename "$PWD")"
+
+# Decide image name based on directory name
+if [ "${DIR_NAME#dev}" != "$DIR_NAME" ]; then
+  IMAGE_TAG="dev-app-slonig-org"
+  echo "Detected dev directory ($DIR_NAME) → using image: $IMAGE_TAG"
+else
+  IMAGE_TAG="app-slonig-org"
+  echo "Detected non-dev directory ($DIR_NAME) → using image: $IMAGE_TAG"
+fi
+
 git checkout master && \
 git pull origin master && \
 export $(cat .env | xargs) && \
-docker build -t app-slonig-org -f docker/Dockerfile \
-  --build-arg IPFS_SERVER=$IPFS_SERVER \
-  --build-arg PEERJS_SERVER=$PEERJS_SERVER \
-  --build-arg COTURN_SERVER=$COTURN_SERVER \
-  --build-arg COTURN_USER=$COTURN_USER \
-  --build-arg COTURN_PASSWORD=$COTURN_PASSWORD \
-  --build-arg AIRDROP_AUTH_TOKEN=$AIRDROP_AUTH_TOKEN \
-  --build-arg MATOMO_SITE_ID=$MATOMO_SITE_ID \
+docker build -t "$IMAGE_TAG" -f docker/Dockerfile \
+  --build-arg IPFS_SERVER="$IPFS_SERVER" \
+  --build-arg PEERJS_SERVER="$PEERJS_SERVER" \
+  --build-arg COTURN_SERVER="$COTURN_SERVER" \
+  --build-arg COTURN_USER="$COTURN_USER" \
+  --build-arg COTURN_PASSWORD="$COTURN_PASSWORD" \
+  --build-arg AIRDROP_AUTH_TOKEN="$AIRDROP_AUTH_TOKEN" \
+  --build-arg MATOMO_SITE_ID="$MATOMO_SITE_ID" \
   . && \
 docker compose up -d
