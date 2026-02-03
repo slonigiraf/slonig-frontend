4532132b769feb78b4a065d812d4808a9368aa08
Reshetov Denis <reshetovdenis@gmail.com>
2026-02-03 22:07:16 +0100

    Towards working unblock

--- packages/page-recommendations/src/Teach/UnblockTutoring.tsx
+++ packages/page-recommendations/src/Teach/UnblockTutoring.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useTranslation } from '../translate.js';
 import { styled } from '@polkadot/react-components';
-import { FullscreenActivity } from '@slonigiraf/slonig-components';
+import { CenterQRContainer, FullscreenActivity, nameFromKeyringPair, ScanQR, SenderComponent, useLoginContext } from '@slonigiraf/slonig-components';
+import { useToggle } from '@polkadot/react-hooks';
 
 interface Props {
   onClose: () => void;
@@ -9,11 +10,34 @@ interface Props {
 
 function UnblockTutoring({ onClose }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
+  const [isQrShown, toggleIsQrShown] = useToggle();
+  const { currentPair } = useLoginContext();
+  const name = nameFromKeyringPair(currentPair);
+
+  const data = JSON.stringify({
+    name,
+  })
 
   return (
-    <FullscreenActivity caption={t('You were a bad tutor')} onClose={onClose} >
+    <FullscreenActivity caption='' onClose={onClose} >
       <StyledDiv>
-        
+        {isQrShown ?
+          <CenterQRContainer>
+            <h1>{t('🚫 Your tutoring is paused')}</h1>
+            <SenderComponent
+              caption={t('Ask a teacher to scan:')}
+              data={data}
+              route={'badges/assess'}
+              textShare={t('Press the link to start tutoring')}
+              onDataSent={toggleIsQrShown}
+            />
+          </CenterQRContainer>
+          :
+          <>
+            <h1>{t('Scan the teacher’s QR code')}</h1>
+            <ScanQR label={t('Scan')} />
+          </>
+        }
       </StyledDiv>
     </FullscreenActivity>
   );
@@ -21,7 +45,7 @@ function UnblockTutoring({ onClose }: Props): React.ReactElement<Props> {
 
 const StyledDiv = styled.div`
   flex: 1;
-  width: 70%;
+  width: 100%;
   min-height: 0;
   display: flex;
   align-items: center;
